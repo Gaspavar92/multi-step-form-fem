24e52e9f172e8450a2798d04e700f29a85342955
Gaspare Tortora <g.tortora2@gmail.com>
2024-08-28 22:59:32 -0400

    Created and styled Plans component

---
+++ src/components/Plans.tsx
@@ -0,0 +1,37 @@
+import { useFormContext } from "./context/Context";
+
+
+const Plans = () => {
+
+    const { recurrence, handlePlan, selectedPlan } = useFormContext();
+
+    const plans = {
+        monthly: [
+            {name: "Arcade", price: "9"},
+            {name: "Advanced", price: "12"},
+            {name: "Pro", price: "15"}
+        ],
+        yearly: [
+            {name: "Arcade", price: "90"},
+            {name: "Advanced", price: "120"},
+            {name: "Pro", price: "150"}
+        ]
+    };
+    
+    return (
+        <div className={`plans ${recurrence} flex gap-8 justify-between`}>
+            {recurrence && plans[recurrence].map(plan => {
+                const isSelected = selectedPlan === plan.name;
+                return (
+                    <div key={plan.name} className={`plan ${plan.name} p-4 flex flex-col w-1/3 border-2 border-[#f0f6ff] hover:border-[#473dff] rounded-md cursor-pointer duration-200`} data-value={plan.name} data-checked={isSelected} onClick={(e) => {handlePlan(e, plan.name, plan.price)}}>
+                        <img src={`../../assets/images/icon-${plan.name}.svg`} className="h-10 w-10 mb-10"></img>
+                        <h2>{plan.name}</h2>
+                        <p className="text-[#9699ab] text-sm">{`$${plan.price}${recurrence === "monthly" ? "/mo" : "/yr"}`}</p>
+                    </div>
+                );
+            })}
+        </div>
+    )
+};
+
+export default Plans;
