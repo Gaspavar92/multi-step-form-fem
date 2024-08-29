1dc48531056f71594e7030df3b299a0bfffe5417
Gaspare Tortora <g.tortora2@gmail.com>
2024-08-28 23:00:09 -0400

    Corrected typo and change p to h2

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -8,7 +8,7 @@ const Sidebar = () => {
     const steps = [
         {number: 1, step: "STEP 1", text: "YOUR INFO"},
         {number: 2, step: "STEP 2", text: "SELECT PLAN"},
-        {number: 3, step: "STEP 3", text: "ADD ONS"},
+        {number: 3, step: "STEP 3", text: "ADD-ONS"},
         {number: 4, step: "STEP 4", text: "SUMMARY"}
     ]
 
@@ -20,7 +20,7 @@ const Sidebar = () => {
                 <div key={step.step} className="first flex gap-6">
                     <div className={`duration-500 ${currentStep === index ? "bg-number number-col" : "bg-transparent text-white"} flex content-center justify-center flex-wrap border border-solid border-white rounded-full h-12 w-12`}>{step.number}</div>
                     <div className="flex flex-col">
-                        <p className="step">{step.step}</p>
+                        <h2 className="step">{step.step}</h2>
                         <p className="text-white">{step.text}</p>
                     </div>
                 </div>
