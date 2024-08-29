37af98fe53a0eda769205dbb6d891447eac9b007
Gaspare Tortora <g.tortora2@gmail.com>
2024-08-28 22:58:43 -0400

    Created new functions to manage state and handle input control

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,9 +1,10 @@
-import { useRef, useState, useEffect } from "react";
+import { useState } from "react";
 import FirstStep from "./FirstStep";
 import SecondStep from "./SecondStep";
 import ThirdStep from "./ThirdStep";
 import FourthStep from "./FourthStep";
 import { useFormContext } from "./context/Context";
+import { Context } from "./context/Context";
 
 const Form = () => {
 
@@ -16,8 +17,13 @@ const Form = () => {
 
     const { currentStep, setCurrentStep } = useFormContext()
     const CurrentStep = steps[currentStep]
+    
+    const [recurrence, setRecurrence] = useState("monthly");
+    const [selectedPlan, setSelectedPlan] = useState("");
 
-    const [formData, setFormData] = useState({});
+    const [formData, setFormData] = useState({
+        recurrence: "monthly"
+    });
 
     const next = (e: React.MouseEvent) => {
         e.preventDefault()
@@ -47,19 +53,46 @@ const Form = () => {
         })
     };
 
+    const handleCheckboxChange = (e: React.ChangeEvent, price) => {
+        const { name, checked } = e.target;
+
+        setFormData((prev) => {
+            if (checked) {
+                return {...prev,
+                        [name]: price
+                }
+            } else {
+                const { [name]: _, ...rest } = prev;
+                return rest;
+            }
+        })
+    };
+
+    const handlePlan = (e: React.MouseEvent, planName, planPrice) => {
+        setSelectedPlan(planName)
+        setFormData((prev) => {
+            return {...prev,
+                "plan": planName,
+                "price": planPrice
+            }
+        })
+    };
+
     const confirm = (e: React.MouseEvent) => {
         e.preventDefault()
         console.log(formData)
     }
 
     return (
-        <form className="col-span-2 p-8 flex flex-col content-center flex-wrap">
-            <div className="steps w-2/3 h-full relative">
-                <CurrentStep onChange={handleChange} />
-                {currentStep > 0 && <button className="back absolute left-0 bottom-0" onClick={back}>Go Back</button>}
-                <button type='submit' className="next absolute right-0 bottom-0 text-white" onClick={currentStep === (steps.length - 1) ? confirm : next}>{currentStep === (steps.length - 1) ? "Confirm" : "Next Step"}</button>
-            </div>
-        </form>
+        <Context.Provider value={{handleChange, handlePlan, recurrence, setRecurrence, selectedPlan, setSelectedPlan, handleCheckboxChange}}>
+            <form className="col-span-2 p-8 flex flex-col content-center flex-wrap">
+                <div className="steps w-10/12 h-full relative">
+                    <CurrentStep />
+                    {currentStep > 0 && <button className="back absolute left-0 bottom-0" onClick={back}>Go Back</button>}
+                    <button type='submit' className="next absolute right-0 bottom-0 text-white" onClick={currentStep === (steps.length - 1) ? confirm : next}>{currentStep === (steps.length - 1) ? "Confirm" : "Next Step"}</button>
+                </div>
+            </form>
+        </Context.Provider>
     );
 };
 
