573547474dcd23d6cfc9256e73356ddc27707f89
Gaspare Tortora <g.tortora2@gmail.com>
2024-08-28 22:57:56 -0400

    Changed second step so that it uses context and changed data gathering logic with event destructuring

--- src/components/SecondStep.tsx
+++ src/components/SecondStep.tsx
@@ -1,4 +1,9 @@
-const SecondStep = ({onChange, onClick}) => {
+import { useFormContext } from "./context/Context";
+import Plans from "./Plans";
+
+const SecondStep = () => {
+
+    const { handleChange, recurrence, setRecurrence, setSelectedPlan } = useFormContext();
 
     return (
         <div className="flex flex-col gap-12">
@@ -6,8 +11,31 @@ const SecondStep = ({onChange, onClick}) => {
                 <h1>Select your plan</h1>  
                 <p>You have the option of monthly or yearly billing.</p>
             </div>
-            <input id="name" name="last-name" onChange={onChange}></input>
-            <label htmlFor="last-name">Last Name</label>
+
+            <Plans />
+            <div className="change-recurrence w-full flex justify-center py-4 bg-[#f0f6ff] rounded-md gap-6">
+
+                <div className={`monthly ${recurrence === "monthly" ? "monthly text-[#02295a]" : "text-[#9699ab]"} font-semibold`}>Monthly</div>
+
+                <label htmlFor="recurrence" className="bg-[#02295a] h-8 w-[65px] flex content-center flex-wrap rounded-full p-1 cursor-pointer">
+                    <input type="checkbox" checked={recurrence === "yearly"} value={recurrence} id="recurrence" name="recurrence" className="hidden" onChange={() => {
+                    
+                    const newRecurrence = recurrence === "monthly" ? "yearly" : "monthly";
+                    setRecurrence(newRecurrence);
+                    setSelectedPlan("");
+                    handleChange({
+                        target: {
+                        name: 'recurrence',
+                        value: newRecurrence
+                        },
+                    });
+                    }}></input>
+                    <div className="toggle h-6 w-6 rounded-full bg-white"></div>
+                </label>
+
+                <div className={`yearly ${recurrence === "yearly" ? "text-[#02295a]" : "text-[#9699ab]"} font-semibold`}>Yearly</div>
+
+            </div>
         </div>
     )
 };
