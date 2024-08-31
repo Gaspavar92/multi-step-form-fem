318fcd023af6383d6ff6618b9e1c95444ed02cb3
Gaspare Tortora <g.tortora2@gmail.com>
2024-08-30 23:33:46 -0400

    Created last step component to recap all selections

--- src/components/FourthStep.tsx
+++ src/components/FourthStep.tsx
@@ -2,7 +2,16 @@ import { useFormContext } from "./context/Context";
 
 const FourthStep = () => {
 
-    const { handleChange } = useFormContext();
+    const { formData, change } = useFormContext();
+
+        const length = formData.recurrence.length;
+        let plan = formData.recurrence[0].toUpperCase();
+        let i = 1;
+        while (i < length) {
+            plan+=formData.recurrence[i];
+            i++;
+        }
+
 
     return (
         <div className="flex flex-col gap-12">
@@ -10,8 +19,32 @@ const FourthStep = () => {
                 <h1>Finishing up</h1>  
                 <p>Double-check everything looks OK before confirming.</p>
             </div>
-            <input id="name" name="address" onChange={handleChange}></input>
-            <label htmlFor="address">Address</label>
+            <div className="final-info flex flex-col gap-4 bg-[#fafbff] p-6 rounded-md">
+                <div className="final-plan-info flex justify-between">
+                    <div className="chosen-plan">
+                        <h2>{formData.plan} ({plan})</h2>
+                        <button className="hover:underline" onClick={change}>Change</button>
+                    </div>
+                    <p className="plan-price">${formData.price}{formData.recurrence === "monthly" ? "/mo" : "/yr"}</p>
+                </div>
+            {formData.addOns && 
+            <>
+                <div className="final-add-ons border-t pt-4 flex flex-col gap-2">
+                        {formData.addOns.map(addOn => {
+                            return (
+                                <div className="selected-add-ons flex justify-between">
+                                    <h3 className="add-on-name">{addOn.name}</h3>
+                                    <p className="add-on-price">+${addOn.price}{formData.recurrence === "monthly" ? "/mo" : "/yr"}</p>
+                                </div>
+                            )
+                        })}
+                    </div>
+                </>}
+            </div>
+                    <div className="total px-6 flex justify-between">
+                        <div className="h3">Total ({formData.recurrence === "monthly" ? "per month" : "per year"})</div>
+                        <p>+${formData.total}{formData.recurrence === "monthly" ? "/mo" : "/yr"}</p>
+                    </div> 
         </div>
     )
 };
