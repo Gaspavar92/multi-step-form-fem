4b0fc8bf77a0d06a9babecb49f5486463c739ba1
Gaspare Tortora <g.tortora2@gmail.com>
2024-09-04 11:10:08 -0400

    Removed edits

--- src/components/FirstStep.tsx
+++ src/components/FirstStep.tsx
@@ -3,14 +3,10 @@ import { useFormContext } from "./context/FormContext";
 
 const FirstStep = () => {
 
-    const { handleChange, errors, setInvalid, formData } = useFormContext();
+    const { handleChange, errors, setInvalid } = useFormContext();
     const [nameInputValid, setNameInputValid] = useState(false);
     const [emailInputValid, setEmailInputValid] = useState(false);
     const [numberInputValid, setNumberInputValid] = useState(false);
-
-    const [name, setName] = useState<string>(formData.name)
-    const [email, setEmail] = useState<string>(formData.email)
-    const [number, setNumber] = useState<string>(formData.number)
 
     useEffect(() => {
         if (nameInputValid && emailInputValid && numberInputValid) {
@@ -32,18 +28,18 @@ const FirstStep = () => {
             <div className="flex flex-col gap-4">
                     <label className="name form-section flex flex-col" htmlFor="name">
                         Name
-                        <input id="name" name="name" onChange={(e) => {handleChange(e); setNameInputValid(e.target.value.length > 0 ? true : false); setName(e.target.value)}} className={`${errors.name ? "border-red-600 focus-visible:border-red-600 active:border-red-600 border-2" : "border"} focus:outline-none`} placeholder="e.g. Stephen King" value={name}></input>
+                        <input id="name" name="name" onChange={(e) => {handleChange(e); setNameInputValid(e.target.value.length > 0 ? true : false)}} className={`${errors.name ? "border-red-600 focus-visible:border-red-600 active:border-red-600 border-2" : "border"} focus:outline-none`} placeholder="e.g. Stephen King"></input>
                         {errors.name && <p className="text-red-700">{errors.name}</p>}
                     </label>
                     <label className="email form-section flex flex-col" htmlFor="email">
                         Email Address
-                        <input id="email" name="email" onChange={(e) => {handleChange(e); setEmailInputValid(e.target.value.length > 0 ? true : false); setEmail(e.target.value)}}  className={`${errors.email ? "border-red-600 focus-visible:border-red-600 active:border-red-600 border-2" : "border"} focus:outline-none`} placeholder="e.g. stephenking@lorem.com" value={email}></input>
+                        <input id="email" name="email" onChange={(e) => {handleChange(e); setEmailInputValid(e.target.value.length > 0 ? true : false)}}  className={`${errors.email ? "border-red-600 focus-visible:border-red-600 active:border-red-600 border-2" : "border"} focus:outline-none`} placeholder="e.g. stephenking@lorem.com"></input>
                         {errors.email && <p className="text-red-700">{errors.email}</p>}
                     </label>
 
                     <label className="number form-section flex flex-col" htmlFor="number">
                         Phone Number
-                        <input id="number" name="number" onChange={(e) => {handleChange(e); setNumberInputValid(e.target.value.length > 0 ? true : false); setNumber(e.target.value)}}  className={`${errors.number ? "border-red-600 focus-visible:border-red-600 active:border-red-600 border-2" : "border"} focus:outline-none`} placeholder="e.g. +1 234 567 890" value={number}></input>
+                        <input id="number" name="number" onChange={(e) => {handleChange(e); setNumberInputValid(e.target.value.length > 0 ? true : false)}}  className={`${errors.number ? "border-red-600 focus-visible:border-red-600 active:border-red-600 border-2" : "border"} focus:outline-none`} placeholder="e.g. +1 234 567 890"></input>
                         {errors.number && <p className="text-red-700">{errors.number}</p>}
                     </label>
             </div>
